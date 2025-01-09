d6fc6d8406fe468269cb9b60b0543432d63b4d40
Artem Abramov1 <artem_abramov1@epam.com>
2025-01-09 21:01:27 +0400

    fix: downgrade to expo 50 for nativewind support

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,11 +18,16 @@ export default function HomeScreen() {
     <>
       <View style={styles.titleContainer}>
         <Pressable onPress={() => console.log("Hello")} className="">
-          <Text className="red-500">Press me</Text>
+          <Text className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Press me
+          </Text>
         </Pressable>
       </View>
       <View style={styles.titleContainer2}>
-        <NativeText className="thumb-sky-500">Theme view</NativeText>
+        <NativeText className="inline-flex items-center bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">
+          <NativeText className="w-2 h-2 me-1 bg-red-500 rounded-full"></NativeText>
+          Unavailable
+        </NativeText>
       </View>
     </>
   );
